7a57f5a4ae827554ed5ec027a78b350064132750
Matt Zimmer <zimmermja@gmail.com>
2022-01-13 22:04:36 +0000

    PUT requests working

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -18,34 +18,45 @@ let roomData = [
   },
 ];
 
-const getRoom = (roomID) => roomData.find((room) => (room.roomID = roomID));
+const updateRoomArr = (params) => {
+  const newArr = roomData.slice();
+  const roomIndex = newArr.findIndex((room) => room.roomID == params.roomID);
+  if (params.startDate) newArr[roomIndex].startDate = params.startDate;
+  if (params.endDate) newArr[roomIndex].endDate = params.endDate;
+  if (params.friendCount) newArr[roomIndex].friendCount = params.friendCount;
+  if (params.roomFormsRatings)
+    newArr[roomIndex].roomFormsRatings = params.roomFormsRatings;
+  return newArr;
+};
 
-const setFriendCount = (int, id = 1) => (getRoom(id).friendCount = int);
+router.get("/", function (req, res) {
+  res.send(roomData);
+});
 
-const setStartDate = (date, id = 1) => (getRoom(id).startDate = date);
-
-const setEndDate = (date, id = 1) => (getRoom(id).endDate = date);
-
-const setRatings = (rArr, id = 1) => (getRoom(id).roomFormsRatings = rArr);
-
-// ================
-// RESPONSES
-// ================
-
-router.post("/", function (req, res) {
-  const retrieving = req.body.retrieve,
-    roomID = req.body.roomID,
-    startDate = req.body.startDate,
-    endDate = req.body.endDate,
-    friendCount = req.body.friendCount,
-    roomFormsRatings = req.body.roomFormsRatings;
-  if (!retrieving) {
-    if (startDate) setStartDate(friendCount, roomID);
-    if (endDate) setEndDate(friendCount, roomID);
-    if (friendCount) setFriendCount(friendCount, roomID);
-    if (roomFormsRatings) setRatings(friendCount, roomID);
-  }
-  res.send(getRoom(roomID));
+router.put("/", function (req, res) {
+  const newArr = updateRoomArr(req.body);
+  roomData = newArr;
+  res.send(roomData);
 });
 
 module.exports = router;
+
+// router.get("/notes", (req, res) => {
+//   res.send(JSON.stringify(roomData);
+// });
+
+// router.post("/", function (req, res) {
+//   const retrieving = req.body.retrieve,
+//     roomID = req.body.roomID,
+//     startDate = req.body.startDate,
+//     endDate = req.body.endDate,
+//     friendCount = req.body.friendCount,
+//     roomFormsRatings = req.body.roomFormsRatings;
+//   if (!retrieving) {
+//     if (startDate) setStartDate(friendCount, roomID);
+//     if (endDate) setEndDate(friendCount, roomID);
+//     if (friendCount) setFriendCount(friendCount, roomID);
+//     if (roomFormsRatings) setRatings(friendCount, roomID);
+//   }
+//   res.send(getRoom(roomID));
+// });
