0538234836484c79a0099bcdec9a233873b36b31
Matt Zimmer <zimmermja@gmail.com>
2022-01-13 23:50:23 +0000

    POST requests working

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 let roomData = [
   {
-    roomID: 1,
+    roomID: 0,
     startDate: "2022-01-12",
     endDate: "2022-01-15",
     friendCount: 3,
     roomFormsRatings: [[], [], []],
   },
   {
-    roomID: 2,
+    roomID: 1,
     startDate: "2022-01-12",
     endDate: "2022-01-18",
     friendCount: 4,
@@ -18,45 +18,47 @@ let roomData = [
   },
 ];
 
-const updateRoomArr = (params) => {
+const addRoom = (par) => {
+  const newRoom = {
+    roomID: roomData.length,
+    startDate: par.startDate,
+    endDate: par.endDate,
+    friendCount: par.friendCount,
+    roomFormsRatings: par.roomFormsRatings,
+  };
   const newArr = roomData.slice();
-  const roomIndex = newArr.findIndex((room) => room.roomID == params.roomID);
-  if (params.startDate) newArr[roomIndex].startDate = params.startDate;
-  if (params.endDate) newArr[roomIndex].endDate = params.endDate;
-  if (params.friendCount) newArr[roomIndex].friendCount = params.friendCount;
-  if (params.roomFormsRatings)
-    newArr[roomIndex].roomFormsRatings = params.roomFormsRatings;
+  newArr.push(newRoom);
+  return newArr;
+};
+
+const updateRoomArr = (par) => {
+  const newArr = roomData.slice();
+  const roomIndex = newArr.findIndex((room) => room.roomID == par.roomID);
+  if (par.startDate) newArr[roomIndex].startDate = par.startDate;
+  if (par.endDate) newArr[roomIndex].endDate = par.endDate;
+  if (par.friendCount) newArr[roomIndex].friendCount = par.friendCount;
+  if (par.roomFormsRatings)
+    newArr[roomIndex].roomFormsRatings = par.roomFormsRatings;
   return newArr;
 };
 
 router.get("/", function (req, res) {
+  console.log(roomData);
+  res.send(roomData);
+});
+
+router.post("/", function (req, res) {
+  const newArr = addRoom(req.body);
+  roomData = newArr;
+  console.log(roomData);
   res.send(roomData);
 });
 
 router.put("/", function (req, res) {
   const newArr = updateRoomArr(req.body);
   roomData = newArr;
+  console.log(roomData);
   res.send(roomData);
 });
 
 module.exports = router;
-
-// router.get("/notes", (req, res) => {
-//   res.send(JSON.stringify(roomData);
-// });
-
-// router.post("/", function (req, res) {
-//   const retrieving = req.body.retrieve,
-//     roomID = req.body.roomID,
-//     startDate = req.body.startDate,
-//     endDate = req.body.endDate,
-//     friendCount = req.body.friendCount,
-//     roomFormsRatings = req.body.roomFormsRatings;
-//   if (!retrieving) {
-//     if (startDate) setStartDate(friendCount, roomID);
-//     if (endDate) setEndDate(friendCount, roomID);
-//     if (friendCount) setFriendCount(friendCount, roomID);
-//     if (roomFormsRatings) setRatings(friendCount, roomID);
-//   }
-//   res.send(getRoom(roomID));
-// });
