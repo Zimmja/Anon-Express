7ead5ffa747c1df3598cb24df0f5df36a6ad5706
Matt Zimmer <zimmermja@gmail.com>
2022-01-14 11:28:32 +0000

    set updating database to use boolean values

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -34,30 +34,30 @@ const addRoom = (par) => {
 const updateRoomArr = (par) => {
   const newArr = roomData.slice();
   const roomIndex = newArr.findIndex((room) => room.roomID == par.roomID);
-  if (par.startDate) newArr[roomIndex].startDate = par.startDate;
-  if (par.endDate) newArr[roomIndex].endDate = par.endDate;
-  if (par.friendCount) newArr[roomIndex].friendCount = par.friendCount;
-  if (par.roomFormsRatings)
+  if (!!par.startDate) newArr[roomIndex].startDate = par.startDate;
+  if (!!par.endDate) newArr[roomIndex].endDate = par.endDate;
+  if (!!par.friendCount) newArr[roomIndex].friendCount = par.friendCount;
+  if (!!par.roomFormsRatings)
     newArr[roomIndex].roomFormsRatings = par.roomFormsRatings;
   return newArr;
 };
 
 router.get("/", function (req, res) {
-  console.log(roomData);
+  // console.log(roomData);
   res.send(roomData);
 });
 
 router.post("/", function (req, res) {
   const newArr = addRoom(req.body);
   roomData = newArr;
-  console.log(roomData);
+  // console.log(roomData);
   res.send(roomData);
 });
 
 router.put("/", function (req, res) {
   const newArr = updateRoomArr(req.body);
   roomData = newArr;
-  console.log(roomData);
+  // console.log(roomData);
   res.send(roomData);
 });
 
